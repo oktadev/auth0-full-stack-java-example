8c76c4ac60faee0453389bd2769a7f699d3e2261
Matt Raible <matt.raible@okta.com>
2021-12-03 15:07:47 -0700

    Generated with JHipster 7.4.1

--- src/main/webapp/app/entities/photo/photo.tsx
+++ src/main/webapp/app/entities/photo/photo.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import InfiniteScroll from 'react-infinite-scroller';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { Link, RouteComponentProps } from 'react-router-dom';
-import { Button, Col, Row, Table } from 'reactstrap';
+import { Button, Table } from 'reactstrap';
 import { openFile, byteSize, Translate, TextFormat, getSortState } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -98,7 +98,7 @@ export const Photo = (props: RouteComponentProps<{ url: string }>) => {
       <h2 id="photo-heading" data-cy="PhotoHeading">
         <Translate contentKey="flickr2App.photo.home.title">Photos</Translate>
         <div className="d-flex justify-content-end">
-          <Button className="mr-2" color="info" onClick={handleSyncList} disabled={loading}>
+          <Button className="me-2" color="info" onClick={handleSyncList} disabled={loading}>
             <FontAwesomeIcon icon="sync" spin={loading} />{' '}
             <Translate contentKey="flickr2App.photo.home.refreshListLabel">Refresh List</Translate>
           </Button>
@@ -111,12 +111,10 @@ export const Photo = (props: RouteComponentProps<{ url: string }>) => {
       </h2>
       <div className="table-responsive">
         <InfiniteScroll
-          pageStart={paginationState.activePage}
-          loadMore={handleLoadMore}
+          dataLength={photoList ? photoList.length : 0}
+          next={handleLoadMore}
           hasMore={paginationState.activePage - 1 < links.next}
           loader={<div className="loader">Loading ...</div>}
-          threshold={0}
-          initialLoad={false}
         >
           {photoList && photoList.length > 0 ? (
             <Table responsive>
@@ -182,7 +180,7 @@ export const Photo = (props: RouteComponentProps<{ url: string }>) => {
                     <td>{photo.taken ? <TextFormat type="date" value={photo.taken} format={APP_DATE_FORMAT} /> : null}</td>
                     <td>{photo.uploaded ? <TextFormat type="date" value={photo.uploaded} format={APP_DATE_FORMAT} /> : null}</td>
                     <td>{photo.album ? <Link to={`album/${photo.album.id}`}>{photo.album.title}</Link> : ''}</td>
-                    <td className="text-right">
+                    <td className="text-end">
                       <div className="btn-group flex-btn-group-container">
                         <Button tag={Link} to={`${match.url}/${photo.id}`} color="info" size="sm" data-cy="entityDetailsButton">
                           <FontAwesomeIcon icon="eye" />{' '}
--- src/main/webapp/app/shared/layout/menus/account.tsx
+++ src/main/webapp/app/shared/layout/menus/account.tsx
@@ -6,7 +6,7 @@ import { Translate, translate } from 'react-jhipster';
 import { getLoginUrl } from 'app/shared/util/url-utils';
 import { NavDropdown } from './menu-components';
 
-const accountMenuItemsAuthenticated = (
+const accountMenuItemsAuthenticated = () => (
   <>
     <MenuItem icon="sign-out-alt" to="/logout" data-cy="logout">
       <Translate contentKey="global.menu.account.logout">Sign out</Translate>
@@ -14,7 +14,7 @@ const accountMenuItemsAuthenticated = (
   </>
 );
 
-const accountMenuItems = (
+const accountMenuItems = () => (
   <>
     <DropdownItem id="login-item" tag="a" href={getLoginUrl()} data-cy="login">
       <FontAwesomeIcon icon="sign-in-alt" /> <Translate contentKey="global.menu.account.login">Sign in</Translate>
@@ -24,7 +24,7 @@ const accountMenuItems = (
 
 export const AccountMenu = ({ isAuthenticated = false }) => (
   <NavDropdown icon="user" name={translate('global.menu.account.main')} id="account-menu" data-cy="accountMenu">
-    {isAuthenticated ? accountMenuItemsAuthenticated : accountMenuItems}
+    {isAuthenticated ? accountMenuItemsAuthenticated() : accountMenuItems()}
   </NavDropdown>
 );
 
